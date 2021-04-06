df2d7afaf2d1d4c62fcd421921069f04cc4b90b1
Puskás Gellért <devgellert@gmail.com>
2021-04-06 20:51:30 +0200

    base layout

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import css from "./GamePage.module.scss";
 
 const GamePage = () => {
-  return <div>My Game.</div>;
+  return (
+    <div className={css["GamePage"]}>
+      <nav className={css["top-nav"]}>Hello</nav>
+      <nav className={css["left-nav"]}>Hello</nav>
+      <nav className={css["bottom-nav"]}>Hello</nav>
+      <section className={css["map-wrap"]}>
+        <img src="/map.jpg" alt="map" />
+      </section>
+    </div>
+  );
 };
 
 export default GamePage;
